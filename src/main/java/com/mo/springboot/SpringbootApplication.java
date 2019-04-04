d09ea8859496a9ee0bcb9c74872985f8a2846b28
长果 <mo@changguodeMacBook-Pro.local>
2019-04-04 16:07:59 +0800

    mac

--- src/main/java/com/mo/springboot/SpringbootApplication.java
+++ src/main/java/com/mo/springboot/SpringbootApplication.java
@@ -1,13 +1,13 @@
-package com.mo.springboot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringbootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
-
-}
+//package com.mo.springboot;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SpringbootApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringbootApplication.class, args);
+//	}
+//
+//}
---
+++ src/main/java/com/mo/springboot/YibuTest.java
@@ -0,0 +1,7 @@
+package com.mo.springboot;
+
+public class YibuTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
